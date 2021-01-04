9f7a80e8b52e72eaef4b8b8af40b56d4cb54e3dd
Nathan Chow <emailme@nathanchow.ca>
2021-01-04 00:07:44 -0500

    update button change color

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,54 +91,52 @@ function createQuestion(){
     test.innerHTML = "<h3>"+question+"</h3>";
     // display the answer options
     // the += appends to the data we started on the line above
-    test.innerHTML += "<input class='hidden' type='checkbox' onclick='selectOne(this.id)' id='0' value='A' name='choices'><label for='0'><div class='btn-check0 btn-style' id='check0'> "+chA+"</div></label><br>";
+    test.innerHTML += "<input class='hidden' type='checkbox' onclick='selectOne(this.id)' id='0' value='A' name='choices'><label for='0'><div class='btn-check0 btn-style' id='check0' onclick='choice0(this.id)'> "+chA+"</div></label><br>";
 
-    test.innerHTML += "<input class='hidden' type='checkbox' onclick='selectOne(this.id)' id='1' value='B' name='choices'><label for='1'><div class='btn-check1 btn-style' id='check1'> "+chB+"</div></label><br>";
+    test.innerHTML += "<input class='hidden' type='checkbox' onclick='selectOne(this.id)' id='1' value='B' name='choices'><label for='1'><div class='btn-check1 btn-style' id='check1' onclick='choice1(this.id)'> "+chB+"</div></label><br>";
 
-    test.innerHTML += "<input class='hidden' type='checkbox' onclick='selectOne(this.id)' id='2' value='C' name='choices'><label for='2'><div class='btn-check2 btn-style' id='check2'> "+chC+"</div></label><br>"
+    test.innerHTML += "<input class='hidden' type='checkbox' onclick='selectOne(this.id)' id='2' value='C' name='choices'><label for='2'><div class='btn-check2 btn-style' id='check2' onclick='choice2(this.id)'> "+chC+"</div></label><br>"
 
-    test.innerHTML += "<input class='hidden' type='checkbox' onclick='selectOne(this.id)' id='3' value='D' name='choices'><label for='3'><div class='btn-check3 btn-style' id='check3'> "+chD+"</div></label><br><br>";
+    test.innerHTML += "<input class='hidden' type='checkbox' onclick='selectOne(this.id)' id='3' value='D' name='choices'><label for='3'><div class='btn-check3 btn-style' id='check3' onclick='choice3(this.id)'> "+chD+"</div></label><br><br>";
 
     test.innerHTML += "<button id='submit-response' onclick='checkAnswer()'>Submit Answer</button>";
+}
 
-    var choice0 = document.getElementById("0");
-    var choice1 = document.getElementById("1");
-    var choice2 = document.getElementById("2");
-    var choice3 = document.getElementById("3");
+// change option0 to light blue
+function choice0(id) {
+    document.getElementById(id).style.backgroundColor = "lightblue";
+    document.getElementById("check1").style.backgroundColor = "white";
+    document.getElementById("check2").style.backgroundColor = "white";
+    document.getElementById("check3").style.backgroundColor = "white";
+}
 
-    choice0.onclick = function() {
-        check0.style.backgroundColor = "lightblue";
-        check1.style.backgroundColor = "white";
-        check2.style.backgroundColor = "white";
-        check3.style.backgroundColor = "white";    
-    }
+// change option1 to light blue
+function choice1(id) {
+    document.getElementById(id).style.backgroundColor = "lightblue";
+    document.getElementById("check0").style.backgroundColor = "white";
+    document.getElementById("check2").style.backgroundColor = "white";
+    document.getElementById("check3").style.backgroundColor = "white";
+}
 
-    choice1.onclick = function() {
-        check1.style.backgroundColor = "lightblue";
-        check0.style.backgroundColor = "white";
-        check2.style.backgroundColor = "white";
-        check3.style.backgroundColor = "white";
-    }
+// change option2 to light blue
+function choice2(id) {
+    document.getElementById(id).style.backgroundColor = "lightblue";
+    document.getElementById("check0").style.backgroundColor = "white";
+    document.getElementById("check1").style.backgroundColor = "white";
+    document.getElementById("check3").style.backgroundColor = "white";
+}
 
-    choice2.onclick = function() {
-        check2.style.backgroundColor = "lightblue";
-        check1.style.backgroundColor = "white";
-        check0.style.backgroundColor = "white";
-        check3.style.backgroundColor = "white";
-    }
-
-    choice3.onclick = function() {
-        check3.style.backgroundColor = "lightblue";
-        check1.style.backgroundColor = "white";
-        check2.style.backgroundColor = "white";
-        check0.style.backgroundColor = "white";
-    }
-
+// change option3 to light blue
+function choice3(id) {
+    document.getElementById(id).style.backgroundColor = "lightblue";
+    document.getElementById("check0").style.backgroundColor = "white";
+    document.getElementById("check1").style.backgroundColor = "white";
+    document.getElementById("check2").style.backgroundColor = "white";
 }
 
 // to select only ONE choice
 function selectOne(id) {
-    
+
     for (var i = 0; i < choices.length; i++) 
     {
         document.getElementById(i).checked = false;
